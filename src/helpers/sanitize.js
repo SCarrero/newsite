24666a514db20969fb08cc1998dfe2f7db2ab412
S Carrero <sherry_carrero@freddiemac.com>
2019-10-18 17:54:54 -0400

    Update sanitize.js

--- src/helpers/sanitize.js
+++ src/helpers/sanitize.js
@@ -1,3 +1,8 @@
 module.exports = function(html) {
-  return html.replace(/<[^>]+>/g, '').replace(/\&[^;]{3,6};/g, '');
+  if (html) {
+    return html.replace(/<[^>]+>/g, '').replace(/\&[^;]{3,6};/g, '');
+  }
+  else {    
+    return "";
+  }
 };
